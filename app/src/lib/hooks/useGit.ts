a08e9a2062e916d382475d7e7dd8278997b17c5e
Jafar Rezaei <sayjeyhi@gmail.com>
2025-09-09 13:24:41 +0200

    fix: coep

--- app/src/lib/hooks/useGit.ts
+++ app/src/lib/hooks/useGit.ts
@@ -58,7 +58,7 @@ export function useGit() {
       const headers: {
         [x: string]: string;
       } = {
-        'User-Agent': 'bolt.diy',
+        'User-Agent': 'Preview UI',
       };
 
       const auth = lookupSavedPassword(url);
